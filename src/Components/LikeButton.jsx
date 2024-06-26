76a0be21ffc40e2a12f82b34bc83c24d7d0d038d
Julian <jjvll90@gmail.com>
2024-06-26 17:51:55 -0500

    finished like button

--- src/Components/LikeButton.jsx
+++ src/Components/LikeButton.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 
-function LikeButton() {
+const emptyHeartImage = "https://img.icons8.com/emoji/48/000000/heart-emoji.png"
+const filledHeartImage = "https://img.icons8.com/emoji/48/000000/heart-emoji.png"
 
+function LikeButton() {
+    const [likes, setLikes] = useState(0);
+
+    return (
+        <div className="like-info">
+            <div className="like-button" onClick={() => setLikes(likes + 1)}>
+                {likes > 0 ? (
+                    <img src={filledHeartImage} alt="filled heart" />
+                ) : (
+                    <img src={emptyHeartImage} alt="empty heart" />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default LikeButton
