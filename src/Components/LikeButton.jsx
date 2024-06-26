112464375ebffca390a06fb332d9924a410efc01
Julian <jjvll90@gmail.com>
2024-06-26 18:57:01 -0500

    refactored likebutton

--- src/Components/LikeButton.jsx
+++ src/Components/LikeButton.jsx
@@ -5,19 +5,20 @@ const emptyHeartImage = "♡";
 const filledHeartImage = "♥";
 
 function LikeButton() {
-    const [likes, setLikes] = useState(0);
+const [heartCount, setHeartCount] = useState(0);
 
-    return (
-        <div className="like-info">
-            <div className="like-button" onClick={() => setLikes(likes + 1)}>
-                {likes > 0 ? (
-                    <img src={filledHeartImage} alt="filled heart" />
-                ) : (
-                    <img src={emptyHeartImage} alt="empty heart" />
-                )}
-            </div>
-        </div>
-    )
+const handleLike = () => setHeartCount(heartCount + 1);
+
+const heartImageSrc = heartCount > 0 ? filledHeartImage : emptyHeartImage;
+
+return (
+<div className="like-info">
+    <div className="like-button" onClick={handleLike}>
+    <img src={heartImageSrc} alt="heart" />
+    </div>
+    <p>{heartCount} likes</p>
+</div>
+);
 }
 
 export default LikeButton
