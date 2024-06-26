a81bbcd22a8229c1cd899f0a41a17970d2a38b06
Julian <jjvll90@gmail.com>
2024-06-26 18:15:47 -0500

    updated sample post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,3 +1,25 @@
 import FollowButton from "./FollowButton";
 import LikeButton from "./LikeButton";
+import WaylonPost from "./WaylonPost";
+import WaylonHead from "./WaylonHead";
+
+function Post() {
+    return (
+        <>
+        <div className="post">
+            <div className="user-info">
+                <WaylonHead />
+                <p>🤠 Cowboy Waylon</p>
+                <FollowButton />
+            </div>
+
+        </div><div>
+                <WaylonPost />
+                <LikeButton />
+            </div>
+        </>
+    )
+}
+
+export default Post
 
